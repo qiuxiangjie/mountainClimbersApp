6513420590bc8c1306f2473b1fdce0454a40ffc2
周健翔 <zhoujianxiang@evergrande.com>
2020-09-28 11:35:30 +0800

    fixed some bug

--- screens/login.js
+++ screens/login.js
@@ -87,6 +87,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     color: '#fff',
+    backgroundColor: 'transparent',
   },
   center: {
     flex: 1,
